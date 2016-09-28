ac4cb0bf31debf83c5b8371834e49b5bb9da3acb
stevemacenski <stevenmacenski@gmail.com>
2016-09-28 10:05:23 -0700

    adding homing program and removing temp file

--- ABB_MultiBot/demos/go_to_home_robot_you're_drunk.py
+++ ABB_MultiBot/demos/go_to_home_robot_you're_drunk.py
@@ -8,7 +8,7 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import FollowJointTrajectoryActionGoal
 import time
 
-def functional(commanded_trajectory):
+def functional():
     pub_gary = rospy.Publisher('/gary/joint_trajectory_action/goal',
      FollowJointTrajectoryActionGoal, queue_size=10)
     pub_rosey = rospy.Publisher('/rosey/joint_trajectory_action/goal',
@@ -17,24 +17,17 @@ def functional(commanded_trajectory):
     time.sleep(4)
 
     rate = rospy.Rate(0.01)
-    i = 0
+
     while not rospy.is_shutdown():
-       for command in commanded_trajectory:
-           i += 1
-           
-           #  define a point
+
            traj_waypoint_1_gary = JointTrajectoryPoint()
            traj_waypoint_1_rosey = JointTrajectoryPoint()
 
-           traj_waypoint_1_gary.positions = command          
-           traj_waypoint_1_rosey.positions = [-1*elem for elem in command]
+           traj_waypoint_1_gary.positions = [0,0,0,0,0,0]       
+           traj_waypoint_1_rosey.positions = [0,0,0,0,0,0]
 
-           traj_waypoint_1_rosey.time_from_start.secs = 0 + 2*i #first move slow
-           traj_waypoint_1_gary.time_from_start.secs = 0 + 2*i
-          
-           #debug in terminal
-           print traj_waypoint_1_gary.positions
-           print traj_waypoint_1_rosey.positions
+           traj_waypoint_1_rosey.time_from_start.secs = 10
+           traj_waypoint_1_gary.time_from_start.secs = 10
            
            #  making message
            message_gary = FollowJointTrajectoryActionGoal()
@@ -56,8 +49,9 @@ def functional(commanded_trajectory):
            
            #  appending up to 100 points
            # ex. for i in enumerate(len(waypoints)): append(waypoints[i])
-           message_gary.goal.trajectory.points = [traj_waypoint_1_gary]
-           message_rosey.goal.trajectory.points = [traj_waypoint_1_rosey]
+           message_gary.goal.trajectory.points.append(traj_waypoint_1_gary)
+           message_rosey.goal.trajectory.points.append(traj_waypoint_1_rosey)
+
           
            #  publishing to ROS node
            pub_gary.publish(message_gary)
@@ -67,23 +61,11 @@ def functional(commanded_trajectory):
            
            if rospy.is_shutdown():
                break
+           break
                
 
 if __name__ == '__main__':
-    commandL = []
-    f = open('traj.txt','r')
-    for line in f:
-        parse_line = line.replace('\n','')
-        parse_line = parse_line.replace(
-        "{joint_names: {'joint_1', 'joint_2', 'joint_3' 'joint_4' 'joint_5', 'joint_6'}, points: ",'')
-        positions_position = parse_line.find('positions')
-        velocities_position = parse_line.find('velocities')
-        
-        position_command = parse_line[positions_position+10:velocities_position-2]
-
-        commandL.append(ast.literal_eval(position_command))
-
     try:
-        functional(commandL)
+        functional()
     except rospy.ROSInterruptException:
         pass
